7ea49a92e02d76e46ec3564d43f27e11719343fd
ByteBard <cwchen123@gmail.com>
2026-07-11 16:47:40 +0800

    Implement NaturalInt

---
+++ NaturalInt.cs
@@ -0,0 +1,94 @@
+using System.Numerics;
+
+namespace DomainSpecificType;
+
+public class NaturalInt
+{
+    private readonly int _value;
+
+    public NaturalInt(int value)
+    {
+        if (value < 0) throw new ArgumentOutOfRangeException(nameof(value), "Invalid natural int");
+        _value = value;
+    }
+
+    public static bool TryCreate(int value, out NaturalInt result)
+    {
+        if (value < 0)
+        {
+            result = null!;
+            return false;
+        }
+
+        result = new NaturalInt(value);
+        return true;
+    }
+
+    public static NaturalInt operator +(NaturalInt a, NaturalInt b)
+    {
+        return new NaturalInt(a._value + b._value);
+    }
+
+    public static NaturalInt operator -(NaturalInt a, NaturalInt b)
+    {
+        if (a._value < b._value) throw new ArgumentOutOfRangeException($"{a} - {b}", "Invalid natural int computation");
+        return new NaturalInt(a._value - b._value);
+    }
+
+    public static NaturalInt operator *(NaturalInt a, NaturalInt b)
+    {
+        return new NaturalInt(a._value * b._value);
+    }
+
+    public static NaturalInt operator /(NaturalInt a, NaturalInt b)
+    {
+        if (b._value == 0.0) throw new DivideByZeroException($"Invalid divisor: {b}");
+        return new NaturalInt(a._value / b._value);
+    }
+
+    public static bool operator ==(NaturalInt a, NaturalInt b)
+    {
+        return a._value == b._value;
+    }
+
+    public static bool operator !=(NaturalInt a, NaturalInt b)
+    {
+        return a._value != b._value;
+    }
+
+    public static bool operator <(NaturalInt a, NaturalInt b)
+    {
+        return a._value < b._value;
+    }
+
+    public static bool operator >(NaturalInt a, NaturalInt b)
+    {
+        return a._value > b._value;
+    }
+
+    public static bool operator <=(NaturalInt a, NaturalInt b)
+    {
+        return a._value <= b._value;
+    }
+
+    public static bool operator >=(NaturalInt a, NaturalInt b)
+    {
+        return a._value >= b._value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is NaturalInt other) return this == other;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return _value.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return _value.ToString();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        TryCatchDemo();
-        TryCreateDemo();
+        NaturalIntTryCatchDemo();
+        NaturalIntTryCreateDemo();
+        NaturalFloatTryCatchDemo();
+        NaturalFloatTryCreateDemo();
     }
 
-    static void TryCatchDemo()
+    static void NaturalIntTryCatchDemo()
+    {
+        try
+        {
+            var a = new NaturalInt(4);
+            var b = new NaturalInt(3);
+            Console.WriteLine(a - b);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e);
+            return;
+        }
+    }
+
+    static void NaturalIntTryCreateDemo()
+    {
+        if (NaturalInt.TryCreate(4, out var a)
+            && NaturalInt.TryCreate(3, out var b))
+        {
+            try
+            {
+                Console.WriteLine(a - b);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                return;
+            }
+        }
+        else
+        {
+            Console.Error.WriteLine("Invalid input");
+            return;
+        }
+    }
+
+    static void NaturalFloatTryCatchDemo()
     {
         try
         {
@@ -23,7 +62,7 @@ class Program
         }
     }
 
-    static void TryCreateDemo()
+    static void NaturalFloatTryCreateDemo()
     {
         if (NaturalFloat.TryCreate(3.6, out var a)
             && NaturalFloat.TryCreate(2.4, out var b))
